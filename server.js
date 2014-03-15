864845ba814dd82f1aff313373779a75b4454039
Tim Fogarty <timothy.fogo@gmail.com>
2014-03-15 20:59:00 +0000

    Implements facebook login

--- server.js
+++ server.js
@@ -1,48 +1,50 @@
 'use strict';
 
-var express = require('express'),
+// Module dependencies.
+var express = require('express'),  
     path = require('path'),
     fs = require('fs'),
-    mongoose = require('mongoose');
+    auth = require('./lib/config/middlewares/authorization'),
+    passport = require('passport');
 
-/**
- * Main application file
- */
-
-// Set default node environment to development
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-
-// Application Config
-var config = require('./lib/config/config');
+var app = express();
 
 // Connect to database
-var db = mongoose.connect(config.mongo.uri, config.mongo.options);
+var db = require('./lib/db/mongo').db;
 
 // Bootstrap models
 var modelsPath = path.join(__dirname, 'lib/models');
 fs.readdirSync(modelsPath).forEach(function (file) {
-  if (/(.*)\.(js$|coffee$)/.test(file)) {
-    require(modelsPath + '/' + file);
-  }
+  require(modelsPath + '/' + file);
 });
 
-// Populate empty DB with sample data
-require('./lib/config/dummydata');
-  
+// Populate empty DB with dummy data
+require('./lib/db/dummydata');
+
+
+// Express Configuration
+require('./lib/config/express')(app, passport, db);
+
 // Passport Configuration
-var passport = require('./lib/config/passport');
+require('./lib/config/passport')(passport);
 
-var app = express();
+// Controllers
+var api = require('./lib/controllers/api'),
+    routes = require('./lib/controllers/routes');
 
-// Express settings
-require('./lib/config/express')(app);
+// Server Routes
+//app.get('/api/all', api.all);
 
-// Routing
-require('./lib/routes')(app);
+// Auth Routes
+require('./lib/controllers/auth')(app, passport);
+
+// Angular Routes
+require('./lib/controllers/routes')(app, passport, auth);
 
 // Start server
-app.listen(config.port, function () {
-  console.log('Express server listening on port %d in %s mode', config.port, app.get('env'));
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Express server listening on port %d in %s mode', port, app.get('env'));
 });
 
 // Expose app
