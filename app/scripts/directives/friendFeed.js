5a655836243e260d44e3d0a0b6e04d63f6e85d80
Jonathan Wilson <johny_wilson@hotmail.co.uk>
2014-03-15 23:02:28 +0000

    drag and drop with old one returning

--- app/scripts/directives/friendFeed.js
+++ app/scripts/directives/friendFeed.js
@@ -1,13 +1,18 @@
 angular.module('cruisecontrolApp').directive('friendFeed', function(){
-	return function(scope, element, attrs){
-		var $friends = $(element).find(".friends");
 
-		$friends.draggable({
-			cursor: "move",
-			stop: function() {
+	return function(scope, element, attrs){
+
+
+		var $feed = $(element).find(".feed");
+
+		//feed.
+
+		// $friends.draggable({
+		// 	cursor: "move",
+		// 	stop: function() {
             	
-            }
-		});
+  //           }
+		// });
 
 		// angular.element(".friends").on('mouseover'(function(event) {
 		// 	alert("awdwadwadwad");
--- app/scripts/directives/friendSideBar.js
+++ app/scripts/directives/friendSideBar.js
@@ -2,12 +2,21 @@ angular.module('cruisecontrolApp').directive('friendSideBar', function(){
 
 
 	return function(scope, element, attrs){
-		var $friends = $(element).find(".friends");
+		var friends = $(element).find(".friends");
 
-		$friends.draggable({
+		friends.draggable({
 			cursor: "move",
 			stop: function() {
+            	angular.element('.feed').html("hi");
+            	var copy = $(this);
+            	var parent = $('<div class="list-group ng-scope" ng-repeat="friend in friends" friend-side-bar=""><a href="#" class="list-group-item friends ui-draggable">' + copy.html() + '</a></div>');	
             	
+			
+
+
+            	console.log(copy);
+            	$(this).remove();
+            	console.log(angular.element('.header').append(parent));
             }
 		});
 
